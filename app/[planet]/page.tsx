afe78bdfa7e947e1f95ee7e9fd4f5a5ffc2502ae
BauTancredi <bautistatancredi@hotmail.com>
2022-12-07 02:50:48 +1100

    Loading the data correctly into planet page

--- app/[planet]/page.tsx
+++ app/[planet]/page.tsx
@@ -1,7 +1,18 @@
 import React from "react";
+import planets from "../../data/planets.json";
 
-const PlanetPage = async () => {
-  return <div>Planets page</div>;
+export async function generateStaticParams() {
+  return planets.map((planet) => ({
+    planet: planet.name.toLowerCase(),
+  }));
+}
+
+const PlanetPage = ({ params }: { params: { planet: string } }) => {
+  const planet = planets.find(
+    (planet) => planet.name.toLowerCase() === params.planet
+  );
+
+  return <div>{planet?.name}</div>;
 };
 
 export default PlanetPage;
--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ export default function Home() {
 
 // TODO
 // [] Types
+// [] Redirect
 // [] Deploy
 // [] Linting
 // [] Test

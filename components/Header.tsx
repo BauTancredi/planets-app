827eff2823e3fd306270a4f89fb21c50f98861f9
BauTancredi <bautistatancredi@hotmail.com>
2022-12-07 02:57:12 +1100

    Simple header to test page rendering

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,7 +1,20 @@
+import Link from "next/link";
 import React from "react";
+import planets from "../data/planets.json";
 
 const Header = () => {
-  return <header className="p-4">Header</header>;
+  return (
+    <header className="p-4" style={{ display: "flex" }}>
+      <h1>Header</h1>
+      <ul style={{ display: "flex" }}>
+        {planets.map((planet) => (
+          <li key={planet.name}>
+            <Link href={`/${planet.name.toLowerCase()}`}>{planet.name}</Link>
+          </li>
+        ))}
+      </ul>
+    </header>
+  );
 };
 
 export default Header;
